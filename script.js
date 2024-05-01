4d910f07bbb015beb9ea659a45d469e50983de1d
Christina Conradt-Eberlin <chrcon01@iba.dk>
2024-05-01 08:07:21 +0200

    quiz javascript

---
+++ script.js
@@ -0,0 +1,128 @@
+
+// Quiz social engineering
+const questions = [
+    {
+        question: "Spørgsmål: Hvad er phishing?",
+        answers: [
+            {text: "a) En fiske metode", isCorrect: false},
+            {text: "b) En måde at organisere data på", isCorrect: false},
+            {text: "c) Et computerspil", isCorrect: false},
+            {text: "d) En type digital svindel, hvor svindlere prøver at lokke personlige oplysninger ud af dig", isCorrect: true}
+        ]
+    },
+    {
+        question: "Spørgsmål: Hvad er smishing?",
+        answers: [
+            {text: "a) Phishing via SMS", isCorrect: true},
+            {text: "b) En dating app", isCorrect: false},
+            {text: "c) Social media phishing", isCorrect: false},
+            {text: "d) En dessert", isCorrect: false}
+        ]
+    },
+    {
+        question: "Spørgsmål: Vælg det svar, der bedst beskriver en phishing e-mail",
+        answers: [
+            {text: "a) E-mail anmoder om følsomme personlige oplysninger", isCorrect: true},
+            {text: "b) E-mail er fra en fiskeforretning", isCorrect: false},
+            {text: "c) E-mailen er uden indhold", isCorrect: false},
+            {text: "d) Der er ingen grammatiske fejl og fremstår professionel", isCorrect: false}
+        ]
+    },
+    {
+        question: "Spørgsmål: Hvordan kan du sikre dig, at en e-mail virkelig er fra den person eller organisation, den hævder at være fra?",
+        answers: [
+            {text: "a) Ved at spørge afsenderen direkte", isCorrect: false},
+            {text: "b) Ved at klikke på links i e-mailen for at bekræfte deres ægthed", isCorrect: false},
+            {text: "c) Kontakte organisationen via et telefonnummer fundet på deres officielle hjemmeside", isCorrect: true},
+            {text: "d) Der er ingen grammatiske fejl og fremstår professionel", isCorrect: false}
+        ]
+    },
+    {
+        question: "Spørgsmål: Hvad skal du gøre hvis du modtager en mistænkelig e-mail?",
+        answers: [
+            {text: "Udføre instrukserne i e-mailen, selvom der bedes om personlige oplysninger", isCorrect: false},
+            {text: "b) Ignorere e-mailen og slette den", isCorrect: true},
+            {text: "c) Videresende e-mailen", isCorrect: false},
+            {text: "d) Spørge afsenderen direkte", isCorrect: false}
+        ]
+    }
+];
+
+const questionElement = document.getElementById("quest");
+const answerButtons = document.getElementById("answer-button");
+const nextButton = document.getElementById("btn_next");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Næste →";
+    nextButton.removeEventListener('click', startQuiz); 
+    nextButton.addEventListener('click', handleNextButton);
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = `${questionNo}. ${currentQuestion.question}`;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        if (answer.isCorrect) {
+            button.dataset.correct = answer.isCorrect;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtons.appendChild(button);
+    });
+}
+
+function resetState() {
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+    nextButton.style.display = "none";
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const isCorrect = selectedButton.dataset.correct === "true";
+    selectedButton.classList.add(isCorrect ? "rigtig" : "forkert");
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("rigtig");
+        }
+        button.disabled = true;
+    });
+    if (isCorrect) {
+        score++;
+    }
+    nextButton.style.display = "block";
+}
+
+    function showScore(){
+        resetState();
+        questionElement.innerHTML = `Din score er ${score} ud af ${questions.length}. (br)
+        Tak, fordi du tog vores quiz! Forhåbentlig har du fået en lille fornemmelse af hvad Social Engineering er, og hvordan du kan beskytte dig selv. Husk altid at tænke dig om.`;
+        nextButton.innerHTML = "Prøv igen";
+        nextButton.style.display =(block);
+        nextButton.removeEventListener('click', handleNextButton);
+        nextButton.addEventListener('click', startQuiz);
+    }
+    
+    function handleNextButton(){
+        currentQuestionIndex++;
+        if(currentQuestionIndex < questions.length -1){
+            currentQuestionIndex++;
+            showQuestion();
+        }else{
+            showScore();      
+        }
+    }
+
+document.addEventListener('DOMContentLoaded', startQuiz);
